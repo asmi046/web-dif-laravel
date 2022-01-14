46a6a53c2c1eebf9b632decc3f5999a07ac0eff6
asmi046 <asmi046@gmail.com>
2022-01-15 02:11:38 +0300

    Правки сидов

--- database/seeders/UslugiSeeder.php
+++ database/seeders/UslugiSeeder.php
@@ -18,6 +18,7 @@ class UslugiSeeder extends Seeder
             'price' => 12000,
             'show' => true,
             'description' => 'Простой сайт без системы управления поможет заявить о Вашей компании в сети и даст посетителям общее представление о Вашем бизнесе. Сайт будет снабжен простым но уникальным дизайном будет иметь форму обратной связи без спама.',
+            'img_name' => 'site-visitka.svg'
         ]);
         
         \Illuminate\Support\Facades\DB::table('uslugi')->insert([
@@ -25,6 +26,7 @@ class UslugiSeeder extends Seeder
             'price' => 15000,
             'show' => true,
             'description' => 'Посадочная страница для продажи товара или услуги, самая важная для клиента информация поданная в максимально удобной форме + призыв к действию. Уникальный дизайн, CTA, тригеры стимулирующие посетителя к совершению действия.',
+            'img_name' => 'site-lending.svg'
         ]);
         
         \Illuminate\Support\Facades\DB::table('uslugi')->insert([
@@ -32,6 +34,7 @@ class UslugiSeeder extends Seeder
             'price' => 18000,
             'show' => true,
             'description' => 'Многостраничный сайт с системой управления (CMS). Неогрвниченные возможности по редактированию и добавлению материалов (текст, фото, видео) а так же дальнейшей доработке сайта. Вы получите уникальный дизайн и настройку SEO плагинов.',
+            'img_name' => 'site-korporativ.svg'
         ]);
         
        \Illuminate\Support\Facades\DB::table('uslugi')->insert([
@@ -39,6 +42,7 @@ class UslugiSeeder extends Seeder
             'price' => 20000,
             'show' => true,
             'description' => 'Рубрицированный каталог продукции с вложенными категориями и метками для ускоренного поиска +поиск по каталогу и настройка дополнительных полей для товара. Уникальный дизайн, система управления (CMS) и настройку SEO плагинов.',
+            'img_name' => 'site-catalog.svg'
         ]);
         
         \Illuminate\Support\Facades\DB::table('uslugi')->insert([
@@ -46,6 +50,7 @@ class UslugiSeeder extends Seeder
             'price' => 25000,
             'show' => true,
             'description' => 'Полноценный интернет магазин с каталогом товаров, корзиной, покупкой в 1 клик. Возможность настройки дополнительных полей для товаров и вывод YML фида для контекстной рекламы или публикации в агрегаторах. Неограниченная возможность доработки.',
+            'img_name' => 'site-shop.svg'
         ]);
     }
 }
